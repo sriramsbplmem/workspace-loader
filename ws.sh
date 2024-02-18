7203439f5804c9239ce80bac3eb0708830828ddd
sriram <s_i_am-r@outlook.com>
2024-02-18 05:28:05 +0000

    added load and unload features.

--- ws.sh
+++ ws.sh
@@ -1,19 +1,18 @@
 #!/bin/env bash
-workdir=$1
 space="$HOME/workspace"
 declare -A folders
 folders["data"]='/run/media/zeatoen/Files/data'
 folders["me"]='/run/media/zeatoen/Files/git/sriramsbplmem/me'
 
-if [[ $workdir &&  `echo $(  ls "$space/.config" )| grep $workdir.json`  ]];then echo "entry present";
-else echo "entry absent";
-		exit;
-fi
 
-cat $space/.config/$1.json
+ensure_entry(){
+	if [[ $workdir &&  `echo $(  ls "$space/.config" )| grep $workdir.json`  ]];then echo "entry present";
+	else echo "entry absent";
+			exit;
+	fi
+}
 
-# mkdir $space/$1
-parse(){
+parse_link(){
 
 
 	while read -r line ;do
@@ -26,10 +25,35 @@ parse(){
 	done
 }
 
-if ! [[ -d $space/$workdir ]];then
-		mkdir $space/$workdir
-		jq   ".contents[]" $space/.config/$1.json |parse
-fi
+workdir=$2
+case $1 in
+
+  load)
+   
+	  
+	ensure_entry
+		if ! [[ -d $space/$workdir ]];then
+				mkdir $space/$workdir
+				jq   ".contents[]" $space/.config/$workdir.json |parse_link
+			else
+				echo "wokspace already present"
+		fi
+	;;
+	unload)
+		if [[ -d $space/$workdir ]]; then
+			rm -r $space/$workdir
+			echo "success."
+			exit
+		fi
+
+	;;
+  *)
+    echo "command not found $1"
+	exit
+	;;
+esac
+
+
 
 
 
